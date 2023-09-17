6498765e2f1e25010a1cfe097a4b85ad371efe6d
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-17 18:31:17 +0200

    Table is being defining... TableHeader is defined. TableBody is next to configure

--- app/database/data-table.tsx
+++ app/database/data-table.tsx
@@ -19,12 +19,43 @@ import {
     TableRow,
   } from "@/components/ui/table"
 
-type Props = {}
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+  }
 
-function databaseTable({}: Props) {
-  return (
-    <div>database Table</div>
+export function DataBaseTable<TData, TValue>({columns, data}:DataTableProps<TData, TValue>){
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return(
+      <div className='rounded-md border-neutral-600'>
+        <Table> 
+            <TableHeader>
+                {table.getHeaderGroups().map(headerGroup => {
+                    return(
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {flexRender(header.column.columnDef.header, header.getContext())}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    )
+                })}
+            </TableHeader>
+            <TableBody>
+                
+            </TableBody>
+        </Table>
+      </div>
   )
 }
 
-export default databaseTable
+// https://ui.shadcn.com/docs/components/data-table
+export default DataBaseTable

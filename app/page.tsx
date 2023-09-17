32412d92f1289a0bce687b28c2dbd7d905402e50
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-17 16:46:09 +0200

    shacdn button installed and importe to main page called 'page.tsx'

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-<div>Tomasz</div>
+    <Button>Click me</Button>
+
+  )
+ 
+}
 // create table link
 // https://www.youtube.com/watch?v=Jgr8JjYOJsU
-  )
-  // https://github.com/Elliott-Chong/shadcn-table-yt
-}
+ // https://github.com/Elliott-Chong/shadcn-table-yt

3b11283491164b62385d993e54169793a47ca7f6
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-18 00:55:11 +0200

    Pagination tested successfully, check line 84-104 in data-table

--- app/database/data-table.tsx
+++ app/database/data-table.tsx
@@ -83,10 +83,25 @@ export function DataBaseTable<TData, TValue>({
       </div>
       {/* PAGINATION */}
       <div className="flex items-center justify-start space-x-2 py-4">
-        <Button variant="outline" size="sm" onClick={()=>{
-          table.previousPage();
-        }}>
-
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            table.previousPage();
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            table.nextPage();
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
         </Button>
       </div>
     </div>

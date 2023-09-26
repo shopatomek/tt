1933c38de95ce3121e9f12feda96a19702d4c3ba
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-26 21:55:28 +0200

    more stying changed in buttons signin and sign out

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -10,9 +10,9 @@ const SignInButton = () => {
     if (session && session.user){
          return(
             <div className='flex gap-4 ml-auto'>
-                <p className='text-white font-bold'>Welcome {session.user.name}</p>
+                <p className='text-white font-bold py-2'>Welcome {session.user.name}</p>
                 <Button className='text-white bg-blue-600 hover:bg-blue-700 ml-auto' onClick={() => signOut()}>
-                    <FcGoogle/>Sign Out
+                    <FcGoogle className='mr-2 size-2xl'/>Sign Out
                 </Button>
 
             </div>
@@ -20,7 +20,7 @@ const SignInButton = () => {
     }
 
   return (
-   <Button className='text-white bg-blue-600 hover:bg-blue-700 ml-auto' onClick={() => signIn()}><FcGoogle className='mr-2 size-20px'/>Sign In</Button>
+   <Button className='text-white bg-blue-600 hover:bg-blue-700 ml-auto' onClick={() => signIn()}><FcGoogle className='mr-2 size-xl'/>Sign In</Button>
   )
 }
 

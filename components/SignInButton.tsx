5ec48df978d8310dac12d43a6a267bbcbb0caf01
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-26 23:05:16 +0200

    user Image added check signinbutton lines 17 to 23

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,27 +1,47 @@
-"use client"
-import { signIn, signOut, useSession } from 'next-auth/react';
-import React from 'react'
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
+import Image from "next/image";
 
 const SignInButton = () => {
-    const {data: session} = useSession();
+  const { data: session } = useSession();
 
-    if (session && session.user){
-         return(
-            <div className='flex gap-4 ml-auto'>
-                <p className='text-white font-bold py-2'>Welcome {session.user.name}</p>
-                <Button className='text-white bg-blue-600 hover:bg-blue-700 ml-auto' onClick={() => signOut()}>
-                    <FcGoogle className='mr-2 size-2xl'/>Sign Out
-                </Button>
-
-            </div>
-         )
-    }
+  if (session && session.user) {
+    return (
+      <div className="flex gap-4 ml-auto">
+        <p className="text-neutral-600 text-sm font-bold ">
+          Welcome
+          <p className="text-neutral-400">{session.user.name}</p>
+        </p>
+        <Image
+          src={session.user.image}
+          height={1}
+          width={50}
+          className="rounded-full h-10 w-10  border: bg-blue-700 border-3"
+          alt=""
+        />
+        <Button
+          className="text-white bg-blue-600 hover:bg-blue-700 ml-auto"
+          onClick={() => signOut()}
+        >
+          <FcGoogle className="mr-2 size-2xl" />
+          Sign Out
+        </Button>
+      </div>
+    );
+  }
 
   return (
-   <Button className='text-white bg-blue-600 hover:bg-blue-700 ml-auto' onClick={() => signIn()}><FcGoogle className='mr-2 size-xl'/>Sign In</Button>
-  )
-}
+    <Button
+      className="text-white bg-blue-600 hover:bg-blue-700 ml-auto"
+      onClick={() => signIn()}
+    >
+      <FcGoogle className="mr-2 size-xl" />
+      Sign In
+    </Button>
+  );
+};
 
-export default SignInButton
+export default SignInButton;

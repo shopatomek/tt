9c7bab7704714c784b1ccacbca8f1286bde14465
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-28 01:00:10 +0200

    Prisma connection to mongoDB tested successfully. When user is logging for the first time in mongodb is responsible. createdAt atribute. Every time user is logging out it will be shown in updatedAt atribute. Another solutions will be implemented

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,102 @@
-import NextAuth from "next-auth/next"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// const handler = NextAuth({
+//   providers:[
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+//     }),
+//   ],
+//   callbacks: {
+//     async signIn({ user, account }) {
+//       // Sprawdź, czy użytkownik istnieje w bazie danych na podstawie adresu e-mail
+//       const existingUser = await prisma.user.findUnique({
+//         where: { email: user.email },
+//       });
+
+//       if (existingUser) {
+//         // Aktualizuj dane użytkownika, jeśli istnieje
+//         await prisma.user.update({
+//           where: { id: existingUser.id },
+//           data: {
+//             name: user.name,
+//             image: user.image,
+//             updatedAt: new Date(),
+//           },
+//         });
+//       } else {
+//         // Dodaj nowego użytkownika do bazy danych
+//         await prisma.user.create({
+//           data: {
+//             name: user.name,
+//             email: user.email,
+//             image: user.image,
+//           },
+//         });
+//       }
+
+//       return user;
+//     },
+
+//   }
+// });
 
 const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-        }),
-    ],
-    callbacks: {
-        async signIn({ user,}) {
-            return user;
-        }
-    }
+  providers:[
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (existingUser) {
+        await prisma.user.update({
+          where: { id: existingUser.id },
+          data: {
+            name: user.name,
+            image: user.image,
+            updatedAt: new Date(),
+          },
+        });
+      } else {
+        await prisma.user.create({
+          data: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          },
+        });
+      }
+
+      return user;
+    },
+    async signOut({ user }) {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (existingUser) {
+        await prisma.user.update({
+          where: { id: existingUser.id },
+          data: {
+            lastLogout: new Date(), // Zapisz aktualną datę i godzinę wylogowania
+          },
+        });
+      }
+
+      return null;
+    },
+  }
 });
+
 
 export { handler as GET, handler as POST };
